99c254870fcd494d6c6039a6c3cf55d99e58bdd4
Swolley <myentity@inwind.it>
2026-04-20 18:19:56 +0200

    feat: enhance Contact model with user handling and validation rules
    - Added temporary user handling in the Contact model to facilitate user creation during contact save.
    - Updated the save method to manage user persistence and relationships.
    - Introduced validation rules for creating and updating contacts.
    - Modified the migration to include unique constraints and foreign key relationships for user_id.
    - Adjusted the contacts table structure to improve data integrity and relationships.

--- database/migrations/2026_04_08_191726_create_contacts_table.php
+++ database/migrations/2026_04_08_191726_create_contacts_table.php
@@ -16,10 +16,11 @@ return new class extends Migration
     {
         Schema::create('contacts', function (Blueprint $table): void {
             $table->id();
-            $table->foreignId('customer_id')->constrained('customers', 'id', 'contacts_customer_id_FK')->nullable(true)->setNullOnDelete();
-            $table->foreignId('user_id')->constrained('users', 'id', 'contacts_users_FK')->nullable(true)->nullOnDelete();
-            $table->string('name');
+            $table->unsignedBigInteger('user_id')->nullable(true)->comment('The user that the contact belongs to');
+            $table->string('name')->unique('contacts_name_UN');
+            // not unique because we allow multiple contacts with the same email
             $table->string('email')->nullable();
+            // not unique because we allow multiple contacts with the same phone
             $table->string('phone')->nullable();
 
             MigrateUtils::timestamps(
@@ -27,6 +28,17 @@ return new class extends Migration
                 hasCreateUpdate: true,
                 hasSoftDelete: true,
             );
+
+            $table->unique(['user_id', 'name', 'deleted_at'], 'contacts_UN');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+        });
+
+        Schema::create('customers_contacts', function (Blueprint $table): void {
+            $table->foreignId('customer_id')->constrained('customers', 'id', 'customers_contacts_customer_id_FK')->cascadeOnDelete();
+            $table->foreignId('contact_id')->constrained('contacts', 'id', 'customers_contacts_contact_id_FK')->cascadeOnDelete();
+            $table->primary(['customer_id', 'contact_id']);
+
+            MigrateUtils::timestamps($table);
         });
     }
 
